3ca0f2cdd9dd66faa81641078c485c71afc7ab73
Jibrilos <webxite23@gmail.com>
2021-04-06 11:38:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, CssBaseline, Box } from "@material-ui/core";
+import Navbar from "./Components/layout/Navbar"
 
-function App() {
+const useStyles = makeStyles({
+  root: {
+  
+    color: (props) => props.color,
+    marginTop:"5px",
+    padding:"0 1px"
+  },
+  box:{}
+});
+
+function App(props) {
+  
+  const classes = useStyles(props);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="xl" className={classes.root}  >
+      <CssBaseline />
+      <Box className={classes.box}>
+        <Navbar />
+      </Box>
+    </Container>
   );
 }
 
